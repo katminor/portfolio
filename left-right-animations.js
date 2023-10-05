931a48f079e19b31fa0d4ef48919765ce4a0b0d1
katminor <57873382+katminor@users.noreply.github.com>
2023-10-04 22:48:13 -0400

    Add files via upload

---
+++ left-right-animations.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // JavaScript to observe when the element is in the viewport
+    const threshold = {
+        threshold: .3, // Adjust this value to control when the transition starts
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            } else {
+                entry.target.classList.remove('active'); // Remove the 'active' class when not in viewport
+            }
+        });
+    }, threshold);
+
+    const animatedElements = document.querySelectorAll('.animate');
+    animatedElements.forEach((element) => {
+        observer.observe(element);
+    });
+});
+
